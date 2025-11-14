b39cd383e798e755ba5eb381fb4ae3cf1849c376
visa-dev <virajsachin33@gmail.com>
2025-11-15 00:02:39 +0530

    apply fix

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,10 +1,18 @@
 "use client"
 
+import React from "react"
 import { SessionProvider } from "next-auth/react"
 
-export function Providers({ children }: { children: React.ReactNode }) {
+interface ProvidersProps {
+  children: React.ReactNode
+}
+
+export function Providers({ children }: ProvidersProps) {
   return (
-    <SessionProvider>
+    <SessionProvider 
+      refetchInterval={0} 
+      refetchOnWindowFocus={false}
+    >
       {children}
     </SessionProvider>
   )
